b65dec92471c5c42fbe7dd42e01afacb3f2217cf
Gastón I. Silva <givanse@gmail.com>
2019-01-04 16:46:43 -0800

    first

--- src/ui/components/GlimmerAppImportNpmEs6/component-test.ts
+++ src/ui/components/GlimmerAppImportNpmEs6/component-test.ts
@@ -1,5 +1,24 @@
 import hbs from '@glimmer/inline-precompile';
-import { setupRenderingTest } from '@glimmer/test-helpers';
+import { render, setupRenderingTest } from '@glimmer/test-helpers';
+import Pretender, {FakeXMLHttpRequest, ResponseHandler, SetupCallback} from 'pretender';
+
+/**
+ *
+ * @param status HTTP response status code
+ * @param payload
+ */
+function json(status: number, payload: any): ResponseHandler {
+  return function(request: FakeXMLHttpRequest) {
+    const data = {photo: `${payload} ${request.params.id}`};
+    return [status, { 'Content-Type': 'text/json' }, JSON.stringify(data)];
+  };
+}
+
+const setup: SetupCallback = function() {
+  this.get('/api/v1/photos/:id', json(200, 'photo: '));
+};
+
+const server = new Pretender(setup);
 
 const { module, test } = QUnit;
 
@@ -7,7 +26,10 @@ module('Component: GlimmerAppImportNpmEs6', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    await this.render(hbs`<GlimmerAppImportNpmEs6 />`);
+    await render(hbs`<GlimmerAppImportNpmEs6 />`);
     assert.equal(this.containerElement.textContent, 'Welcome to Glimmer!\n');
+
+    const response = await fetch('/api/v1/photos/300');
+    assert.equal(response.status, 200);
   });
 });
